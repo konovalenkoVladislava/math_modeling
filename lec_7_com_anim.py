5e3cbc4ddd19a16bd7112826cc315186e3c7ec20
konovalenkoVlad <92105859+konovalenkoVladislava@users.noreply.github.com>
2022-03-17 15:22:11 +0000

    ,

--- lec_7_com_anim.py
+++ lec_7_com_anim.py
@@ -3,23 +3,26 @@ import numpy as np
 from matplotlib.animation import FuncAnimation
 
 fig,ax = plt.subplots()
-boll,=plt.plot([], [], 'o', color='r', label='Boll')
+ball,=plt.plot([], [], '0', color='r', label='Boll')
 
-def circle _move (R ,vx0, vy, time):
+def circle_move (R ,vx0, vy0, time):
     x0 = vx0 * time
     y0 = vy0 * time
     alpha = np.arange (0, 2*np.pi, 0.1)
     x= x0 + R*np.cos(alpha)
-    y= y0 + R*np.cos(alpha)
+    y= y0 + R*np.sin(alpha)
     return x,y
 
 edge = 3
 plt.axis('equal')
 ax.set_xlim(-edge, edge)
-
+ax.set_ylim(-edge, edge)
 def animate(i) :
-    ball.set_data(circle_move(R=5, vx0=0.01, xy0=0.01, time=i))
+    ball.set_data(circle_move(R=0.5, vx0=0.01, vy0=0.01, time=i))
 
-ani = animation.FuncAnimation(
-  
-)
+ani=FuncAnimation(fig,
+                 animate,
+                 frames=100,
+                 interval=100
+                 )
+ani.save("lec_7_complex_animation.gif")
